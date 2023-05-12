8cb73dee443dda6070a48d1635a7c5bd3342536f
Evynne <evynneengdev@gmail.com>
2023-05-12 09:45:32 -0300

    criando projeto vue

---
+++ vqr-web/src/components/login.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+<p>E-mail:</p>
+<input type="email">
+<p>Senha:</p>
+<input type="password"/>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
