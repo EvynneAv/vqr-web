70983881e7d3d5e9c7192385d7e140a58f60cc75
Evynne <evynneengdev@gmail.com>
2023-05-15 12:45:34 -0300

    criando o projeto strapi

---
+++ front-end/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/login.vue';
+</script>
+
+<template>
+
+  <login/>
+
+</template>
+
+<style scoped>
+
+</style>
